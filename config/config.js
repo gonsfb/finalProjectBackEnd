4d6e21e11846354bc21b9b0dbc2df73f73965c00
Gons <gonza_af7@gmail.com>
2024-10-24 14:29:59 -0600

    config file changed for deployment

--- config/config.js
+++ config/config.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
 
-
-module.exports={
-
+module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.Db_HOST,
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
 };
- 
