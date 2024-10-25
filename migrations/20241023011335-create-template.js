b93620395d3ec75734290df6c87c87e145fc731c
Gons <97714282+gonsfb@users.noreply.github.com>
2024-10-24 22:59:49 -0600

    Update 20241023011335-create-template.js

--- migrations/20241023011335-create-template.js
+++ migrations/20241023011335-create-template.js
@@ -18,36 +18,50 @@ module.exports = {
       },
       authorId: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
+        references: {
+          model: 'Users',  // Ensure that 'Users' table exists before this migration
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
       },
       // Custom fields (up to 4 single-line strings)
-      custom_string1_state: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      custom_string1_question: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      custom_string2_state: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      custom_string2_question: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      // (Add the other custom fields like multi-line texts, integers, checkboxes...)
+      custom_string1_state: Sequelize.BOOLEAN,
+      custom_string1_question: Sequelize.STRING,
+      custom_string2_state: Sequelize.BOOLEAN,
+      custom_string2_question: Sequelize.STRING,
+      custom_string3_state: Sequelize.BOOLEAN,
+      custom_string3_question: Sequelize.STRING,
+      custom_string4_state: Sequelize.BOOLEAN,
+      custom_string4_question: Sequelize.STRING,
+      // Custom fields (up to 4 multi-line texts)
+      custom_text1_state: Sequelize.BOOLEAN,
+      custom_text1_question: Sequelize.STRING,
+      custom_text2_state: Sequelize.BOOLEAN,
+      custom_text2_question: Sequelize.STRING,
+      custom_text3_state: Sequelize.BOOLEAN,
+      custom_text3_question: Sequelize.STRING,
+      custom_text4_state: Sequelize.BOOLEAN,
+      custom_text4_question: Sequelize.STRING,
+      // Custom fields (up to 4 positive integers)
+      custom_int1_state: Sequelize.BOOLEAN,
+      custom_int1_question: Sequelize.STRING,
+      custom_int2_state: Sequelize.BOOLEAN,
+      custom_int2_question: Sequelize.STRING,
+      custom_int3_state: Sequelize.BOOLEAN,
+      custom_int3_question: Sequelize.STRING,
+      custom_int4_state: Sequelize.BOOLEAN,
+      custom_int4_question: Sequelize.STRING,
       // Custom fields (up to 4 checkboxes)
-      custom_checkbox1_state: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      custom_checkbox1_question: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      // Timestamps
+      custom_checkbox1_state: Sequelize.BOOLEAN,
+      custom_checkbox1_question: Sequelize.STRING,
+      custom_checkbox2_state: Sequelize.BOOLEAN,
+      custom_checkbox2_question: Sequelize.STRING,
+      custom_checkbox3_state: Sequelize.BOOLEAN,
+      custom_checkbox3_question: Sequelize.STRING,
+      custom_checkbox4_state: Sequelize.BOOLEAN,
+      custom_checkbox4_question: Sequelize.STRING,
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -58,7 +72,9 @@ module.exports = {
       }
     });
   },
+
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Templates');
   }
 };
+
